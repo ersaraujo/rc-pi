0efd3cc613c582f8bc993310b7a090e90b6d2510
ersaraujo <ersa@cin.ufpe.br>
2024-08-07 22:33:29 -0300

    remove request and add robot communication

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from proto import RComm_pb2 as rcomm
 TEAM_NAME = "ROBOCIN"
 
 MULTICAST_GROUP = '224.0.0.1'
+ROBOT_IP = '199.0.1.1'
 SEND_PORT = 19900
 RECEIVE_PORT = 19901
 
@@ -35,25 +36,52 @@ def receive_response(sock):
         print("timout")
         send_request(sock)
 
+def packet_available(sock):
+    while True:
+        data, _ = sock.recvfrom(1024)
+        
+        if data:
+            msg = rcomm.SSLSpeed()
+            
+            return True, msg
+        
+        return False, None
+
+def send2robot(msg, sock):
+    serialized_msg = msg.SerializeToString()
+    sock.sendto(serialized_msg, (ROBOT_IP, SEND_PORT))
+
 def main():
     ip_pc = None
     
-    # Configura o socket para enviar mensagens
-    send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    send_sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
-    
-    receive_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    receive_sock.bind(('', RECEIVE_PORT))
-    receive_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    
+    pc_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     mreq = struct.pack('4sl', socket.inet_aton(MULTICAST_GROUP), socket.INADDR_ANY)
-    receive_sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    
+    pc_sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
+    pc_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    pc_sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    
+    pc_sock.bind(('', RECEIVE_PORT))    
+
+    robot_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    robot_sock.setsockopt(socket.SOL_SOCKET, 25, struct.pack('16s', b'eth0'))
     
     while ip_pc is None:
-        ip_pc = receive_response(receive_sock)
+        ip_pc = receive_response(pc_sock)
 
-    send_sock.close()
-    receive_sock.close()
+    while True:
+        try:
+            newMsg, msg = packet_available(pc_sock)
+        
+            if newMsg:
+                print(f"Received from {ip_pc}:")
+                send2robot(msg, robot_sock)
+
+        except KeyboardInterrupt:
+            break
+
+    pc_sock.close()
+    robot_sock.close()
 
 if __name__ == "__main__":
     main()
